31c2ac1e16727de3512ceeca268f93d64530da37
Jungle Smashfist <gearl00taz@gmail.com>
2025-09-13 18:33:08 +0300

    tests v2

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -28,7 +28,7 @@ describe("[UNIT]: auth", () => {
         const res = targets
             .map(headers => getAPIKey(headers))
             .filter(key => key === null);
-        expect(res.length).toBe(6)
+        expect(res.length).toBe(2)
     });
 
     test("APIKEY on valid", () => {
