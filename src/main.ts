0f09799504bd144568d59d7797a4620e12acccc8
Jungle Smashfist <gearl00taz@gmail.com>
2025-09-17 19:52:47 +0300

    failing eslint pipeline

--- src/main.ts
+++ src/main.ts
@@ -48,3 +48,8 @@ app.use("/v1", v1Router);
 app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
 });
+
+
+function unusedLintCItest() {
+  // does nothing but triggerts eslint
+}
